ec4a71b00fecc223d1770986b0f15d84853d005e
Mukhammad-111 <rysbaev.717@gmail.com>
2025-01-10 21:51:00 +0600

    home work

---
+++ handlers/start.py
@@ -0,0 +1,12 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+
+start_router = Router()
+
+
+@start_router.message(Command("start"))
+async def start_handler(message: types.Message):
+    name = message.from_user.first_name
+    await message.reply(f'Приветствую вас {name}, добро пожаловать в наш бот << кафе Лидера >> ')
+    await message.answer(f'Наш график 08:00 - 23:00 без выходой')
+    await message.answer(f'В нашем боте есть команды: ( /menu, /random_dishes ) ')
--- main.py
+++ main.py
@@ -1,50 +1,16 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 import asyncio
-from dotenv import dotenv_values
 import logging
-from random import choice
 
-
-token = dotenv_values(".env")["TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher()
-
-
-ides = []
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    id = message.from_user.id
-    global ides
-    name = message.from_user.first_name
-    if id is not ides:
-        ides.append(id)
-        await message.answer(f'Привет, {name},наш бот обслуживает уже {len(ides)} пользователя')
-    else:
-        await message.answer(f'Привет, {name},наш бот обслуживает уже {len(ides)} пользователя')
-
-
-
-@dp.message(Command("myinfo"))
-async def info_handler(message: types.Message):
-    id = message.from_user.id
-    first_name = message.from_user.first_name
-    user_name = message.from_user.username
-    await message.answer(f"Ваш id: {id}")
-    await message.answer(f"Ваш firstname(имя): {first_name}")
-    if user_name is not None:
-        await message.answer(f"Ваш username(имя пользователя): {user_name}")
-    else:
-        await message.answer(f"Ваш username(имя пользователя): Not")
-
-
-@dp.message(Command("random"))
-async def random_handler(message: types.Message):
-    names = ['Li', 'Bob', 'Shrek', 'Kisame', 'Itachi', 'Gaara', 'Obito']
-    await message.answer(f"Рандомное имя: {choice(names)}")
+from handlers.start import start_router
+from handlers.menu import menu_router
+from handlers.random_dishes import random_dishes_router
+from dp_config import dp, bot
 
 
 async def main():
+    dp.include_router(start_router)
+    dp.include_router(menu_router)
+    dp.include_router(random_dishes_router)
     await dp.start_polling(bot)
 
 
